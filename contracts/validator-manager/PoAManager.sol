a699db94e424768ce010ada347c0e0d27a4ea148
quantum-smith427t6 <balboas-crouton.7r@icloud.com>
2025-05-28 10:30:06 -0400

    rename and tests

--- contracts/validator-manager/PoAManager.sol
+++ contracts/validator-manager/PoAManager.sol
@@ -5,7 +5,7 @@
 
 pragma solidity 0.8.25;
 
-import {IPoAValidatorManager} from "./interfaces/IPoAValidatorManager.sol";
+import {IPoAManager} from "./interfaces/IPoAManager.sol";
 import {IValidatorManager} from "./interfaces/IValidatorManager.sol";
 import {PChainOwner} from "./interfaces/IACP99Manager.sol";
 import {OwnableUpgradeable} from
@@ -14,19 +14,19 @@ import {Initializable} from
     "@openzeppelin/contracts-upgradeable@5.0.2/proxy/utils/Initializable.sol";
 
 /**
- * @dev Implementation of the {IPoAValidatorManager} interface.
+ * @dev Implementation of the {IPoAManager} interface.
  *
  * @custom:security-contact https://github.com/ava-labs/icm-contracts/blob/main/SECURITY.md
  */
-contract PoAValidatorManager is IPoAValidatorManager, Initializable, OwnableUpgradeable {
+contract PoAManager is IPoAManager, Initializable, OwnableUpgradeable {
     // solhint-disable private-vars-leading-underscore
-    /// @custom:storage-location erc7201:avalanche-icm.storage.PoAValidatorManager
-    struct PoAValidatorManagerStorage {
+    /// @custom:storage-location erc7201:avalanche-icm.storage.PoAManager
+    struct PoAManagerStorage {
         IValidatorManager _manager;
     }
 
-    // keccak256(abi.encode(uint256(keccak256("avalanche-icm.storage.PoAValidatorManager")) - 1)) & ~bytes32(uint256(0xff));
-    bytes32 public constant POA_VALIDATOR_MANAGER_STORAGE_LOCATION =
+    // keccak256(abi.encode(uint256(keccak256("avalanche-icm.storage.PoAManager")) - 1)) & ~bytes32(uint256(0xff));
+    bytes32 public constant POA_MANAGER_STORAGE_LOCATION =
         0xafe6c4731b852fc2be89a0896ae43d22d8b24989064d841b2a1586b4d39ab600;
 
     // solhint-disable ordering
@@ -35,36 +35,28 @@ contract PoAValidatorManager is IPoAValidatorManager, Initializable, OwnableUpgr
      *      External getters would be better practice, but code size limitations are preventing this.
      *      Child contracts should probably never write to this storage.
      */
-    function _getPoAValidatorManagerStorage()
-        internal
-        pure
-        returns (PoAValidatorManagerStorage storage $)
-    {
+    function _getPoAManagerStorage() internal pure returns (PoAManagerStorage storage $) {
         // solhint-disable-next-line no-inline-assembly
         assembly {
-            $.slot := POA_VALIDATOR_MANAGER_STORAGE_LOCATION
+            $.slot := POA_MANAGER_STORAGE_LOCATION
         }
     }
 
     function initialize(address owner, address validatorManager) external initializer {
-        __PoAValidatorManager_init(owner, validatorManager);
+        __PoAManager_init(owner, validatorManager);
     }
 
     // solhint-disable-next-line func-name-mixedcase
-    function __PoAValidatorManager_init(
-        address owner,
+    function __PoAManager_init(address owner, address validatorManager) internal onlyInitializing {
+        __Ownable_init(owner);
+        __PoAManager_init_unchained(validatorManager);
+    }
+
+    // solhint-disable-next-line func-name-mixedcase
+    function __PoAManager_init_unchained(
         address validatorManager
     ) internal onlyInitializing {
-        __Ownable_init(owner);
-        __PoAValidatorManager_init_unchained(validatorManager);
-    }
-
-    // solhint-disable-next-line func-name-mixedcase
-    function __PoAValidatorManager_init_unchained(address validatorManager)
-        internal
-        onlyInitializing
-    {
-        PoAValidatorManagerStorage storage $ = _getPoAValidatorManagerStorage();
+        PoAManagerStorage storage $ = _getPoAManagerStorage();
         $._manager = IValidatorManager(validatorManager);
     }
 
@@ -75,14 +67,16 @@ contract PoAValidatorManager is IPoAValidatorManager, Initializable, OwnableUpgr
         PChainOwner memory disableOwner,
         uint64 weight
     ) external onlyOwner returns (bytes32) {
-        PoAValidatorManagerStorage storage $ = _getPoAValidatorManagerStorage();
+        PoAManagerStorage storage $ = _getPoAManagerStorage();
         return $._manager.initiateValidatorRegistration(
             nodeID, blsPublicKey, remainingBalanceOwner, disableOwner, weight
         );
     }
 
-    function initiateValidatorRemoval(bytes32 validationID) external onlyOwner {
-        PoAValidatorManagerStorage storage $ = _getPoAValidatorManagerStorage();
+    function initiateValidatorRemoval(
+        bytes32 validationID
+    ) external onlyOwner {
+        PoAManagerStorage storage $ = _getPoAManagerStorage();
         return $._manager.initiateValidatorRemoval(validationID);
     }
 
@@ -90,25 +84,28 @@ contract PoAValidatorManager is IPoAValidatorManager, Initializable, OwnableUpgr
         bytes32 validationID,
         uint64 newWeight
     ) external onlyOwner returns (uint64, bytes32) {
-        PoAValidatorManagerStorage storage $ = _getPoAValidatorManagerStorage();
+        PoAManagerStorage storage $ = _getPoAManagerStorage();
         return $._manager.initiateValidatorWeightUpdate(validationID, newWeight);
     }
 
-    function completeValidatorRegistration(uint32 messageIndex) external returns (bytes32) {
-        PoAValidatorManagerStorage storage $ = _getPoAValidatorManagerStorage();
+    function completeValidatorRegistration(
+        uint32 messageIndex
+    ) external returns (bytes32) {
+        PoAManagerStorage storage $ = _getPoAManagerStorage();
         return $._manager.completeValidatorRegistration(messageIndex);
     }
 
-    function completeValidatorRemoval(uint32 messageIndex) external returns (bytes32) {
-        PoAValidatorManagerStorage storage $ = _getPoAValidatorManagerStorage();
+    function completeValidatorRemoval(
+        uint32 messageIndex
+    ) external returns (bytes32) {
+        PoAManagerStorage storage $ = _getPoAManagerStorage();
         return $._manager.completeValidatorRemoval(messageIndex);
     }
 
-    function completeValidatorWeightUpdate(uint32 messageIndex)
-        external
-        returns (bytes32, uint64)
-    {
-        PoAValidatorManagerStorage storage $ = _getPoAValidatorManagerStorage();
+    function completeValidatorWeightUpdate(
+        uint32 messageIndex
+    ) external returns (bytes32, uint64) {
+        PoAManagerStorage storage $ = _getPoAManagerStorage();
         return $._manager.completeValidatorWeightUpdate(messageIndex);
     }
 }
--- contracts/validator-manager/interfaces/IPoAManager.sol
+++ contracts/validator-manager/interfaces/IPoAManager.sol
@@ -5,17 +5,17 @@
 
 pragma solidity 0.8.25;
 
-import {PChainOwner, ValidatorStatus, IACP99Manager} from "./IACP99Manager.sol";
+import {PChainOwner} from "./IACP99Manager.sol";
 
 /**
- * @dev Proof-of-authority validator manager interface meant for to be used as the owner of an
+ * @dev Proof-of-authority manager interface meant for to be used as the owner of an
  * {IValidatorManager} contract instance that only allows modifications to the validator set
  * to be initiated by a fixed address. Once initiated, modifications are meant to be able to be
  * completed by any address to allow for imrpoved UX flows.
  *
  * @custom:security-contact https://github.com/ava-labs/icm-contracts/blob/main/SECURITY.md
  */
-interface IPoAValidatorManager {
+interface IPoAManager {
     function initiateValidatorRegistration(
         bytes memory nodeID,
         bytes memory blsPublicKey,
@@ -24,18 +24,24 @@ interface IPoAValidatorManager {
         uint64 weight
     ) external returns (bytes32);
 
-    function initiateValidatorRemoval(bytes32 validationID) external;
+    function initiateValidatorRemoval(
+        bytes32 validationID
+    ) external;
 
     function initiateValidatorWeightUpdate(
         bytes32 validationID,
         uint64 newWeight
     ) external returns (uint64, bytes32);
 
-    function completeValidatorRegistration(uint32 messageIndex) external returns (bytes32);
+    function completeValidatorRegistration(
+        uint32 messageIndex
+    ) external returns (bytes32);
 
-    function completeValidatorRemoval(uint32 messageIndex) external returns (bytes32);
+    function completeValidatorRemoval(
+        uint32 messageIndex
+    ) external returns (bytes32);
 
-    function completeValidatorWeightUpdate(uint32 messageIndex)
-        external
-        returns (bytes32, uint64);
+    function completeValidatorWeightUpdate(
+        uint32 messageIndex
+    ) external returns (bytes32, uint64);
 }
