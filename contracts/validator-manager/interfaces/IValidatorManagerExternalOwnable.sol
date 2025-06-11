ea01fd920f2ca633cc0213a8d15d8403fa37b6da
quantum-smith427t6 <balboas-crouton.7r@icloud.com>
2025-06-11 11:20:39 -0400

    add natspec comment

--- contracts/validator-manager/interfaces/IValidatorManagerExternalOwnable.sol
+++ contracts/validator-manager/interfaces/IValidatorManagerExternalOwnable.sol
@@ -3,6 +3,12 @@ pragma solidity 0.8.25;
 
 import {IValidatorManager} from "./IValidatorManager.sol";
 
+/**
+ * @notice Combines the {IValidatorManager} interface with the external ownership transfer function from {Ownable}.
+ * @dev This interface is intended for contracts that need to interact with both validator management and ownership transfer
+ *      without importing both {IValidatorManager} and {Ownable} separately. Note that {Ownable} is a contract, not an interface,
+ *      so this interface only exposes the external ownership transfer method ({transferOwnership}) for convenience.
+ */
 interface IValidatorManagerExternalOwnable is IValidatorManager {
     /**
      * @notice Transfers ownership of the validator manager to a new owner.
